1a4e6f53b3cf9d92aed6f6bc0b172bff40f32dce
Abelitho <abelito.gilbert@gmail.com>
2023-08-14 21:24:27 +0700

    NEW

---
+++ app/src/main/java/com/freshism/app/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,245 @@
+package com.freshism.app.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.freshism.app.R;
+import com.freshism.app.databinding.FragmentDashboardBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class DashboardFragment extends Fragment {
+
+    private FragmentDashboardBinding binding;
+    private DatabaseReference manualRef;
+    private DatabaseReference autoRef;
+    private DatabaseReference debuRef;
+    private DatabaseReference udaraRef;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    int debu = 0;
+    int udara = 0;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Inisialisasi referensi ke Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        debuRef = database.getReference("sensor/debu");
+        udaraRef = database.getReference("sensor/udara");
+        manualRef = database.getReference("mode/manual/value");
+        autoRef = database.getReference("mode/otomatis/value");
+
+        binding.progressBar2.show();
+
+        // Ngebaca data debu dari Firebase
+        debuRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Double nilaiDebu = dataSnapshot.getValue(Double.class);
+                if (nilaiDebu != null) {
+                    debu = (int) Math.round(nilaiDebu);
+                    if (binding!=null && binding.txtDebu!=null){
+                        binding.txtDebu.setText(String.valueOf(debu));
+                    }
+
+                    // Pengecekan hasil status udara & debu
+                    String status = getStatus();
+                    if (binding !=null && binding.txtStatus !=null){
+                        binding.txtStatus.setText(status);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
+            }
+        });
+
+        // Baca data udara dari Firebase
+        udaraRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Double nilaiUdara = dataSnapshot.getValue(Double.class);
+                if (nilaiUdara != null) {
+                    udara = (int) Math.round(nilaiUdara);
+                    if (binding!=null && binding.txtUdara!=null){
+                        binding.txtUdara.setText(String.valueOf(udara));}
+
+                    // Pengecekan hasil status udara & debu
+                    String status = getStatus();
+                    if (binding!=null && binding.txtStatus!=null){
+                        binding.txtStatus.setText(status);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
+            }
+        });
+
+        // Button ke firebase
+        // Manual
+        // Manual Switch
+        binding.switchManual.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (buttonView.isPressed()) {
+                    if (isChecked) {
+                        // Save nilai boolean "true" ke database, tombol "ON" diklik
+                        manualRef.setValue("1");
+                        if (getContext() != null) {
+                            Toast.makeText(getContext(), "Value set to ON", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        // Save nilai boolean "false" ke database, tombol "OFF" diklik
+                        manualRef.setValue("0");
+                        if (getContext() != null) {
+                            Toast.makeText(getContext(), "Value set to OFF", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+
+
+        // Otomatis
+        binding.switchAuto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    if (binding!=null && binding.switchAuto !=null) {
+                        autoRef.setValue(1);
+                        if (getContext() != null) {
+                            Toast.makeText(getContext(), "Value set to ON", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } else {
+                    String status = getStatus();
+                    if (status.equals("TIDAK SEHAT")) {
+                        if (binding!= null && binding.switchAuto!=null) {
+                            binding.switchAuto.setChecked(true);
+                            if (getContext() != null) {
+                                Toast.makeText(getContext(), "Value set to ON", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    } else {
+                        autoRef.setValue(0);
+                        if (getContext() != null) {
+                            Toast.makeText(getContext(), "Value set to OFF", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+
+// Ambil nilai boolean dari database saat aplikasi dimulai
+        manualRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                if (value != null) {
+                    if (value.equals("1")) {
+                        // Nilai true (ON), tombol "ON" diklik
+                        if (binding != null) {
+                            binding.switchManual.setChecked(true);
+                        }
+                    } else if (value.equals("0")) {
+                        // Nilai false (OFF), tombol "OFF" diklik
+                        if (binding != null) {
+                            binding.switchManual.setChecked(false);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error
+            }
+        });
+
+
+
+        // Ambil nilai boolean dari database saat aplikasi dimulai
+        autoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Integer value = snapshot.getValue(Integer.class);
+                if (value != null) {
+                    if (value.equals(1) || getStatus().equals("TIDAK SEHAT")) {
+                        // Nilai true (ON), tombol "ON" diklik
+                        if (binding!=null) {
+                            binding.switchAuto.setChecked(true);
+                        }
+                    } else {
+                        // Nilai false (OFF), tombol "OFF" diklik
+                        if (binding != null) {
+                            binding.switchAuto.setChecked(false);
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error
+            }
+        });
+    }
+
+    private String getStatus() {
+        String result = "";
+
+        if (udara >= 0 && udara <= 50) {
+            if (debu >= 0 && debu <= 50) {
+                result = "SEHAT";
+            } else if (debu >= 51 && debu <= 150) {
+                result = "TIDAK SEHAT";
+            }
+        } else if (udara >= 51 && udara <= 100) {
+            if (debu >= 0 && debu <= 50) {
+                result = "SEHAT";
+            } else if (debu >= 51 && debu <= 150) {
+                result = "SEHAT";
+            } else if (debu >= 151) {
+                result = "TIDAK SEHAT";
+            }
+        } else if (udara >= 100) {
+            if (debu >= 0 && debu <= 50) {
+                result = "TIDAK SEHAT";
+            } else if (debu >= 51 && debu <= 150) {
+                result = "TIDAK SEHAT";
+            } else if (debu >= 151) {
+                result = "TIDAK SEHAT";
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/freshism/app/ui/home/HomeFragment.java
@@ -0,0 +1,162 @@
+package com.freshism.app.ui.home;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.freshism.app.PreferencesController;
+import com.freshism.app.R;
+import com.freshism.app.databinding.FragmentHomeBinding;
+import com.freshism.app.ui.dashboard.DashboardFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    int debu = 0;
+    int udara = 0;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String email = PreferencesController.getEmail(getContext());
+        binding.useremail.setText(email);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference debuRef = database.getReference("sensor/debu");
+        DatabaseReference udaraRef = database.getReference("sensor/udara");
+        Handler handler = new Handler();
+
+        String statusInfo = getString(R.string.status_info_bar);
+        binding.txtExplaination.setText(statusInfo);
+
+        // Ambil data dari Firebase Realtime Database realtime
+        debuRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (binding != null) {
+                    Double doubleValue = dataSnapshot.getValue(Double.class);
+                    if (doubleValue != null) {
+                        // Ambil nilai dari dataSnapshot ke TextView
+                        debu = (int) Math.round(doubleValue);
+
+                        // Ambil status
+                        String status = getStatus().first;
+                        // Ambil deskripsi hasil
+                        String deskripsi = getStatus().second;
+
+                        binding.txtExplaination.setText(deskripsi);
+                        binding.btnStatus.setText(status);
+                    } else {
+                        // Handle jika nilai double dari dataSnapshot bernilai null
+                        // Misalnya, tampilkan pesan kesalahan atau lakukan tindakan sesuai kebutuhan
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle jika ada error
+            }
+        });
+
+// Ambil data dari Firebase Realtime Database secara realtime untuk debu dan udara
+        udaraRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (binding != null) {
+                    Double doubleValue = dataSnapshot.getValue(Double.class);
+                    if (doubleValue != null) {
+                        udara = (int) Math.round(doubleValue);
+
+                        // Ambil status dan deskripsi hasil
+                        Pair<String, String> statusPair = getStatus();
+                        String status = statusPair.first;
+                        String deskripsi = statusPair.second;
+
+                        binding.txtExplaination.setText(deskripsi);
+                        binding.btnStatus.setText(status);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle jika ada error
+            }
+        });
+
+
+
+        binding.btnAnalysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity()!=null){
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction().replace(R.id.containerCustomer, new DashboardFragment())
+                        .commit();
+                }
+            }
+        });
+    }
+
+    private Pair<String, String> getStatus() {
+        String result = "";
+        String deskripsi = "";
+        if (udara >= 0 && udara <= 50 && debu >= 0 && debu <= 50) {
+            result = "Sehat";
+            deskripsi="Kualitas udara dirumah Anda SEHAT. Udara relatif bersih dengan sedikit partikel debu.";
+        } else if (udara >= 0 && udara <= 50 && debu >= 51 && debu <= 150) {
+            result = "Sehat";
+            deskripsi="Kualitas udara di rumah Anda SEHAT. Kebersihan udara relatif bersih dan partikel debu berada berada pada level sedang.";
+        } else if (udara >= 51 && udara <= 100 && debu >= 0 && debu <= 50) {
+            result = "Sehat";
+            deskripsi ="Kualitas udara di rumah Anda SEHAT. Kebersihan udara berada pada level sedang dengan partikel debu relatif sedikit.";
+        } else if (udara >= 51 && udara <= 100 && debu >= 51 && debu <= 150) {
+            result = "Sehat";
+            deskripsi="Kualitas udara di rumah Anda SEHAT. Kebersihan udara dan jumlah partikel debu berada di level sedang.";
+        } else if (udara >= 0 && udara <= 50 && debu >= 151) {
+            result = "Tidak Sehat";
+            deskripsi="Kualitas udara di rumah Anda TIDAK SEHAT. Kebersihan udara relatif bersih akan tetapi jumlah partikel debu banyak. Jagalah kebersihan rumah Anda dan nyalakan Freshism.";
+        } else if (udara >= 51 && udara <= 100 && debu >= 151) {
+            result = "Tidak Sehat";
+            deskripsi="Kualitas udara di rumah Anda TIDAK SEHAT. Kebersihan udara relatif sedang, dan jumlah partikel debu banyak. Atur sirkulasi udara dan jagalah kebersihan rumah Anda. Segera nyalakan Freshism.";
+        } else if (udara >= 100 && debu >= 0 && debu <= 50) {
+            result = "Tidak Sehat";
+            deskripsi ="Kualitas udara di rumah Anda TIDAK SEHAT. Kebersihan udara buruk dan jumlah partikel debu relatif sedikit. Atur sirkulasi udara dan segera nyalakan Freshism.";
+        } else if (udara >= 100 && debu >= 51 && debu <= 150) {
+            result = "Tidak Sehat";
+            deskripsi ="Kualitas udara di rumah Anda TIDAK SEHAT. Kebersihan udara buruk dan jumlah partikel debu relatif sedang. Tetap atur sirkulasi udara dan jaga kebersihan rumah. Segera nyalakan Freshism.";
+        } else if (udara >= 100 && debu >= 151) {
+            result = "Tidak Sehat";
+            deskripsi ="Kualitas udara dirumah Anda TIDAK SEHAT. Kebersihan udara buruk dan jumlah partikel udara banyak. Segera nyalakan Freshism";
+        }
+        return new Pair<>(result, deskripsi);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
